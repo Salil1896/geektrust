e1a9a63d8ee676a9c764c258970dcd8733abec3a
Salil Mamodiya <salil.mamodiya@Spider.local>
2021-06-05 16:13:01 +0530

    boundary case handling of last emi

--- src/main/java/com/example/utils/LoanUtils.java
+++ src/main/java/com/example/utils/LoanUtils.java
@@ -7,7 +7,10 @@ import com.example.models.LoanPaymentType;
 import com.example.models.LumpSumPayment;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author salil.mamodiya
@@ -24,17 +27,32 @@ public class LoanUtils {
     }
 
     public static Integer balancePayment(Loan loan, List<LoanPayment> loanPayments, Integer emiId) {
-        Integer emiAmount = loan.getEmi().getAmount() * emiId;
-        Integer lumpSumAmount = (int) Math.ceil(loanPayments.stream()
-                .filter(x -> LoanPaymentType.LUMP_SUM.equals(x.getType()))
-                .filter(x -> ((LumpSumPayment) x).getEmiId() <= emiId)
-                .collect(Collectors.toList()).stream().map(LoanPayment::getAmount).reduce(0.0, Double::sum));
+        Double loanAmount = totalLoanAmount(loan);
+        Map<Integer, List<LumpSumPayment>> emiIdVsLoanPayments =
+                loanPayments.stream().filter(x -> LoanPaymentType.LUMP_SUM.equals(x.getType())).map(x -> (LumpSumPayment) x)
+                        .collect(Collectors.groupingBy(LumpSumPayment::getEmiId));
 
-        return emiAmount + lumpSumAmount;
+        AtomicReference<Double> currentPayment = new AtomicReference<>(0.0);
+        IntStream.range(1, emiId + 1).forEach(id -> {
+            double emi = (double) loan.getEmi().getAmount();
+            double paymentAfterEmi = currentPayment.get() + emi;
+            if (Double.compare(paymentAfterEmi, loanAmount) > 0) {
+                emi = loanAmount - currentPayment.get();
+            }
+            currentPayment.set(currentPayment.get() + Math.ceil(emi));
+
+            if (emiIdVsLoanPayments.containsKey(id)) {
+                currentPayment.set(currentPayment.get() +
+                        emiIdVsLoanPayments.get(id).stream().map(LoanPayment::getAmount).reduce(0.0, Double::sum));
+            }
+        });
+
+        return (int) Math.ceil(currentPayment.get());
     }
 
     public static Integer emiLeft(Loan loan, Integer balancePayment) {
         Double amountLeft = totalLoanAmount(loan) - balancePayment;
+        if (amountLeft <= 0.0) return 0;
         return (int) Math.ceil((double) amountLeft / loan.getEmi().getAmount());
     }
 
